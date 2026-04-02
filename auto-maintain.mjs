53523ee56d4b4b5b9fa3e27278b8cdc906837889
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:50:14 -0500

    style: js-beautify formatting (auto-maintain.mjs)

--- auto-maintain.mjs
+++ auto-maintain.mjs
@@ -4,76 +4,75 @@ const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 const WEEK = 7 * DAY;
 
-export default function maintain(server){
+export default function maintain(server) {
 
-void async function AutomaticRestart() {
+  void async function AutomaticRestart() {
 
-  while (true) {
-    await sleep(MINUTE);
-    if (!server.listening) {
-      try{
-      server.listen(3000);
-      }catch(e){
-        console.log(e.message);
-        continue;
+    while (true) {
+      await sleep(MINUTE);
+      if (!server.listening) {
+        try {
+          server.listen(3000);
+        } catch (e) {
+          console.log(e.message);
+          continue;
+        }
       }
+
     }
 
-  }
+  }?.();
 
-}?.();
+  void async function ClearIdleConnections() {
 
-void async function ClearIdleConnections() {
+    while (true) {
+      await sleep(HOUR);
+      if (server.listening) {
+        server.closeIdleConnections();
+      }
 
-  while (true) {
-    await sleep(HOUR);
-    if (server.listening) {
-      server.closeIdleConnections();
     }
 
-  }
+  }?.();
 
-}?.();
+  void async function DailyClearAllConnections() {
 
-void async function DailyClearAllConnections() {
+    while (true) {
+      await sleep(DAY);
+      if (server.listening) {
+        server.closeAllConnections();
+      }
 
-  while (true) {
-    await sleep(DAY);
-    if (server.listening) {
-      server.closeAllConnections();
     }
 
-  }
+  }?.();
 
-}?.();
+  void async function WeeklyReboot() {
 
+    while (true) {
+      await sleep(WEEK);
+      if (server.listening) {
+        server.close(Ͱ => server.listen(3000));
+      }
 
-void async function WeeklyReboot() {
-
-  while (true) {
-    await sleep(WEEK);
-    if (server.listening) {
-      server.close(Ͱ=>server.listen(3000));
     }
 
-  }
-
-}?.();
+  }?.();
 
   void async function MonthlyHardReboot() {
 
-  while (true) {
-    await sleep(WEEK);
-    await sleep(WEEK);
-    await sleep(WEEK);
-    await sleep(WEEK);
-    
-    server.listen(3000);
-    server.listen(3000);
+    while (true) {
+      await sleep(WEEK);
+      await sleep(WEEK);
+      await sleep(WEEK);
+      await sleep(WEEK);
 
-  }
+      server.listen(3000);
+      server.listen(3000);
 
-}?.();
+    }
+
+  }?.();
 
 }
 
